b7b74e3f5fb52924b92f3600cf6638d0b5f1e0de
Alpha Chen <achen@pivotal.io>
2016-02-12 10:23:57 -0800

    Adding a single new player to the ladder [#113590843]

--- lib/ocean_shores/ladder.rb
+++ lib/ocean_shores/ladder.rb
@@ -21,6 +21,14 @@ module OceanShores
       elsif !winner_position && !loser_position
         new_players << winner
         new_players << loser
+      elsif !winner_position && loser_position
+        total_ladder_length = new_players.size
+        distance_between_loser_and_end = total_ladder_length - loser_position
+        place_to_add_winner = total_ladder_length - (distance_between_loser_and_end / 2).to_i
+        new_players = new_players.insert(place_to_add_winner, winner)
+        return Ladder.new(new_players)
+      elsif winner_position && !loser_position
+        return Ladder.new(new_players << loser)
       end
       Ladder.new(new_players)
     end
--- test/test_ladder.rb
+++ test/test_ladder.rb
@@ -20,4 +20,13 @@ class TestLadder < Minitest::Test
     assert_equal [123, 456], ladder.add_match(winner: 123, loser: 456).players
     assert_equal [456, 123], ladder.add_match(winner: 456, loser: 123).players
   end
+
+  def test_only_two_players_one_new_player
+    ladder = Ladder.new([123, 456])
+
+    assert_equal [123, 789, 456], ladder.add_match(winner: 789, loser: 123).players
+    assert_equal [123, 456, 789], ladder.add_match(winner: 789, loser: 456).players
+    assert_equal [123, 456, 789], ladder.add_match(winner: 123, loser: 789).players
+    assert_equal [123, 456, 789], ladder.add_match(winner: 456, loser: 789).players
+  end
 end
