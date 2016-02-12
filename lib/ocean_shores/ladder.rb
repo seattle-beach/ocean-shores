dc23de3b4143a76b76ca35e7e7ae99c46b32ef51
Alpha Chen <achen@pivotal.io>
2016-02-12 10:13:02 -0800

    Implement adding a match for two existing players [#113590843]

--- lib/ocean_shores/ladder.rb
+++ lib/ocean_shores/ladder.rb
@@ -8,8 +8,20 @@ module OceanShores
 
     def add_match(winner:, loser:)
       new_players = @players.dup
-      new_players << winner
-      new_players << loser
+      winner_position = new_players.index(winner)
+      loser_position = new_players.index(loser)
+      if winner_position && loser_position
+        if winner_position < loser_position
+          return Ladder.new(new_players)
+        elsif winner_position - loser_position == 1
+          new_players[winner_position] = loser
+          new_players[loser_position] = winner
+          return Ladder.new(new_players)
+        end
+      elsif !winner_position && !loser_position
+        new_players << winner
+        new_players << loser
+      end
       Ladder.new(new_players)
     end
   end
--- test/test_ladder.rb
+++ test/test_ladder.rb
@@ -1,4 +1,4 @@
-require 'minitest/autorun'
+require_relative 'test_helper'
 
 require 'ocean_shores/ladder'
 
@@ -13,5 +13,11 @@ class TestLadder < Minitest::Test
     ladder = empty_ladder.add_match(winner: 123, loser: 456)
     assert_empty empty_ladder.players
     assert_equal [123, 456], ladder.players
+  end
+
+  def test_only_two_players
+    ladder = Ladder.new([123, 456])
+    assert_equal [123, 456], ladder.add_match(winner: 123, loser: 456).players
+    assert_equal [456, 123], ladder.add_match(winner: 456, loser: 123).players
   end
 end
